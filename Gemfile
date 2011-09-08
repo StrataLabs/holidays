f17bb711bbee3d94c8fe408e667672b384c71495
Tejas Dinkar <tejas@gja.in>
2011-09-08 15:38:50 +0530

    added spork to speed up the slow build

--- Gemfile
+++ Gemfile
@@ -2,8 +2,6 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.1.0'
 
-gem 'sqlite3'
-
 group :assets do
   gem 'sass-rails', "  ~> 3.1.0"
   gem 'coffee-rails', "~> 3.1.0"
@@ -18,6 +16,7 @@ gem "mongoid"
 gem 'haml'
 
 group :test, :development do
+  gem 'spork'
   gem "rspec-rails", "~> 2.6"
   gem "factory_girl_rails"
   gem "guard-coffeescript"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,20 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
+require 'rubygems'
+require 'spork'
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+Spork.prefork do
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
 
-RSpec.configure do |config|
-  config.mock_with :rspec
-  config.before(:each) do
-    ObjectSpace.each_object(Class) { |x| x.destroy_all if x.included_modules.include?(Mongoid::Document) }
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+  RSpec.configure do |config|
+    config.mock_with :rspec
+    config.before(:each) do
+      ObjectSpace.each_object(Class) { |x| x.destroy_all if x.included_modules.include?(Mongoid::Document) }
+    end
   end
+end
+
+Spork.each_run do
 end

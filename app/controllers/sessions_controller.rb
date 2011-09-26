e6b39ac1f8db1225b2855526be1bf626ec8b006d
Tejas Dinkar <tejas@gja.in>
2011-09-26 20:57:44 +0530

    Sending the user back to the page he came from

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,6 @@ class SessionsController < ApplicationController
   def facebook
     data = request.env["omniauth.auth"]
     User.create_from_facebook_auth(data)
-    render :text => "foo"
+    redirect_to (request.env["omniauth.origin"] || root_path)
   end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -8,9 +8,19 @@ describe SessionsController do
 
     it "creates a user from facebook OAuth" do
       get :facebook
-      response.should be_ok
       user = User.first(:conditions => {:provider => "facebook", :uid => "uid"})
       user.name.should == "Full Name"
+    end
+
+    it "redirects to the url defined by omniauth" do
+      request.env["omniauth.origin"] = "/foo/bar"
+      get :facebook
+      response.should redirect_to("/foo/bar")
+    end
+
+    it "redirects back to root if no origin is specified" do
+      get :facebook
+      response.should redirect_to("/")
     end
   end
 end
